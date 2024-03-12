0af1c1fa71c8921f92b163b547595926cac4a38d
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2024-03-12 23:24:32 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/sign_up_page.dart';
+import 'package:flutter/material.dart';
+
+import 'ui/pages/splash_page.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/get-started': (context) => GetStartedPage(),
+        '/sign-up': (context) => SignUpPage(),
+      },
+    );
+  }
+}
