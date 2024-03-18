2c79d561e8782f89fbebc0f515a8cd11bcad3632
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2024-03-18 14:59:56 +0700

    create homePage header

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,69 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import '../../shared/theme.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          right: defaultMargin,
+          left: defaultMargin,
+          top: 30,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Howdy,\nKezia Anne',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semibold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Where to fly today?',
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: light,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/image_profile.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
+    );
+  }
+}
--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:airplane/ui/pages/home_page.dart';
 import 'package:airplane/ui/widget/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
 import '../../shared/theme.dart';
@@ -9,6 +10,10 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget buildContent() {
+      return HomePage();
+    }
+
     Widget customBottomNavigation() {
       return Align(
         alignment: Alignment.bottomCenter,
@@ -50,7 +55,7 @@ class MainPage extends StatelessWidget {
       backgroundColor: kBackgroundColor,
       body: Stack(
         children: [
-          Text('Main Page'),
+          buildContent(),
           customBottomNavigation(),
         ],
       ),
