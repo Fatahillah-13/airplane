1083699460d8d89cc7fe6e6373467c836f54e0a4
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2024-04-19 09:39:07 +0700

    add destination_tile

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:airplane/ui/widget/destination_card.dart';
+import 'package:airplane/ui/widget/destination_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import '../../shared/theme.dart';
@@ -106,10 +107,64 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget newDestination() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+          left: defaultMargin,
+          right: defaultMargin,
+          bottom: 140,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'New This Year',
+              style: blackTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: semibold,
+              ),
+            ),
+            DestinationTile(
+              name: 'Danau Beratan',
+              city: 'Singaraja',
+              imageURL: 'assets/image_destination6.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Sydney Opera',
+              city: 'Australia',
+              imageURL: 'assets/image_destination7.png',
+              rating: 4.7,
+            ),
+            DestinationTile(
+              name: 'Roma',
+              city: 'Italy',
+              imageURL: 'assets/image_destination8.png',
+              rating: 4.8,
+            ),
+            DestinationTile(
+              name: 'Payung Teduh',
+              city: 'Singapore',
+              imageURL: 'assets/image_destination9.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Hili Hey',
+              city: 'Monaco',
+              imageURL: 'assets/image_destination10.png',
+              rating: 4.7,
+            ),
+          ],
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
         popularDestination(),
+        newDestination(),
       ],
     );
   }
